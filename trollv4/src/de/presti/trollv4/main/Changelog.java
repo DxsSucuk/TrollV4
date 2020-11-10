ef9f94ae76ed49292967561b2ac6e7772ffdd151
Presti <Presti@Presti.fritz.box>
2020-11-10 16:04:02 +0100

    4.4.6 Changelog

--- trollv4/src/de/presti/trollv4/main/Changelog.java
+++ trollv4/src/de/presti/trollv4/main/Changelog.java
@@ -25,6 +25,8 @@ public class Changelog {
 	public static void log(Player p) {
 		p.sendMessage(Data.prefix + "Changelog:");
 		p.sendMessage(Data.prefix + "Version » " + Data.version);
+		p.sendMessage(Data.cu + "Big Error Fix!");
+		p.sendMessage(Data.prefix + "Version » 4.4.5");
 		p.sendMessage(Data.cu + "Hotfix");
 		p.sendMessage(Data.prefix + "Version » 4.4.4");
 		p.sendMessage(Data.cp + "AutoUpdater");
