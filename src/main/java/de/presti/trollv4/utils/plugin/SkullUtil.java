3766fb936a00cbbea2723ed6456ed9813654466a
Presti <dxssucuk@hotmail.com>
2024-08-12 14:38:15 +0200

    Bug fix for Skulls.

--- src/main/java/de/presti/trollv4/utils/plugin/SkullUtil.java
+++ src/main/java/de/presti/trollv4/utils/plugin/SkullUtil.java
@@ -1,5 +1,6 @@
 package de.presti.trollv4.utils.plugin;
 
+import com.cryptomorin.xseries.XMaterial;
 import com.cryptomorin.xseries.profiles.builder.XSkull;
 import com.cryptomorin.xseries.profiles.objects.Profileable;
 import org.bukkit.inventory.ItemStack;
@@ -9,10 +10,13 @@ import java.util.UUID;
 public class SkullUtil {
 
     public static ItemStack getSkull(UUID uuid) {
+        if (uuid == null) {
+            return XMaterial.PLAYER_HEAD.parseItem();
+        }
         try {
             return XSkull.createItem().profile(Profileable.of(uuid)).apply();
         } catch (Exception e) {
-            return XSkull.createItem().apply();
+            return XMaterial.PLAYER_HEAD.parseItem();
         }
     }
 
